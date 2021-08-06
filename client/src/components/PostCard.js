6a10dc4bf63fcf2723dc4cf11dff589b0a45c8dc
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-06 12:38:51 +0530

    delete post function created

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 function PostCard({ post: { body, createdAt, id, userName, likeCount, commentCount, likes } }) {
 
@@ -24,7 +25,7 @@ function PostCard({ post: { body, createdAt, id, userName, likeCount, commentCou
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <LikeButton user={user} post={{id, likes, likeCount}} />
+                <LikeButton user={user} post={{ id, likes, likeCount }} />
                 <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
                     <Button color='blue' basic>
                         <Icon name='comment' />
@@ -34,11 +35,7 @@ function PostCard({ post: { body, createdAt, id, userName, likeCount, commentCou
                     </Label>
                 </Button>
                 {
-                    user && user.userName === userName && (
-                        <Button as="div" color="red" floated="right" onClick={() => console.log("Delete")}>
-                            <Icon name="trash" style={{margin: 0}}/>
-                        </Button>
-                    )
+                    user && user.userName === userName && <DeleteButton postId={id} />
                 }
             </Card.Content>
         </Card>
